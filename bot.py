18971032a10c84686fad9f0f4cf2ceb2a2d4c812
senzxyz2xxx <senzxyz2006@gmail.com>
2026-06-26 05:35:03 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ ALLOWED_CHANNELS = [
 # ============================
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 # เก็บประวัติแชทแยกต่อ channel
 chat_sessions = {}
